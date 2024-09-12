6708debaf598f3cf00a2c9d1b4123c48d1a3f143
Paul Obinna <paulobinna493@gmail.com>
2024-09-12 04:31:51 +0100

    fixed arrows

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// const headerHeroOne = (document.querySelector(
+//   " .header-hero .header-hero-one h1"
+// ).innerHTML = "<h1>Obi is a boy</h1>");
+// console.log(headerHeroOne)
+
+// const headerHeroOne = (document.querySelector(
+//   ".header-hero .header-hero-one h1"
+// ).textContent = "obi is a boy");
+// console.log(headerHeroOne);
