da9190e9a6fd26a1a9dac094b8429ea3d5fb7fad
Paul Obinna <paulobinna493@gmail.com>
2024-11-18 17:02:37 +0100

    added functionality to the hamburger

--- js/app.js
+++ js/app.js
@@ -7,3 +7,11 @@
 //   ".header-hero .header-hero-one h1"
 // ).textContent = "obi is a boy");
 // console.log(headerHeroOne);
+const hamburger = document.querySelector(".hamburger");
+const menuIcon = document.querySelector(".img-hamburger");
+const xIcon = document.querySelector(".x-icon");
+const menu = document.querySelector(".menu");
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
